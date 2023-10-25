bc5f03e350d5a1cc8ea7047710ad7cc6404df18c
Andrey Rocha <ar@tsl.io>
2023-10-25 07:51:31 -0300

    Added Angular Material UI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { CalculadoraPjPfComponent } from './components/calculadora-pj-pf/calcula
 import { CalculadoraPjComponent } from './components/calculadora-pj/calculadora-pj.component';
 import { CalculadoraPfComponent } from './components/calculadora-pf/calculadora-pf.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
